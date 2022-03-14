020ee1ba5c093a617d59ebd52ce955444c199db5
Malher96 <97679570+Malher96@users.noreply.github.com>
2022-03-14 15:16:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(map);
